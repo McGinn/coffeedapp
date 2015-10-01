8a3f8ad36866472db210a49154d92f55b3214b4f
McGinn <alexander.mcginn@gmail.com>
2015-09-30 23:30:38 -0400

    MAIN back to BASE

--- coffeedapp/settings.py
+++ coffeedapp/settings.py
@@ -64,14 +64,14 @@ WSGI_APPLICATION = 'coffeedapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-if ON_HEROKU == True:
+if ON_HEROKU == 1:
     import dj_database_url
     DATABASES = {'default': dj_database_url.config()}
 else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(MAIN_DIR, 'db.sqlite3'),
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
 
